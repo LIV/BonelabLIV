c859b76ca3b06dd627ee35154aa2d2e8e95f4c27
Raicuparta <Raicuparta@users.noreply.github.com>
2022-12-06 13:15:15 +0100

    Bump version 1.0.1

--- BonelabLIV/Properties/AssemblyInfo.cs
+++ BonelabLIV/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using MelonLoader;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
 // MelonLoader
-[assembly: MelonInfo(typeof(BonelabLivMod), "LIV", "1.0.0", "Raicuparta")]
+[assembly: MelonInfo(typeof(BonelabLivMod), "LIV", "1.0.1", "Raicuparta")]
 [assembly: MelonGame]
